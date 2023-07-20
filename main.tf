fb8e8ab59f815aedd473654d5a05f21362025a3b
lukaboy-linux <lukaboy.ccz@gmail.com>
2023-07-19 23:23:50 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.16"
+        }
+    }
+    required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name = "ubuntu"
+
+  tags = {
+    Name = "PrimeiraInstancia"
+  }
+}
