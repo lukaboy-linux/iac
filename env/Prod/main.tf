346b90f1007812a6ad267ead843848a046986f65
lukaboy-linux <lukaboy.ccz@gmail.com>
2023-07-28 23:53:58 -0300

    add prod

--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -1,6 +1,11 @@
-#module "aws-prod" {
-#  source = "../../infra"
-#  instancia = "t2.micro"
-#  regiao_aws = "us-east-1"
-#  chave = "IAC-PROD"
-#}
+module "aws-prod" {
+  source = "../../infra"
+  instancia = "t2.micro"
+  regiao_aws = "us-east-1"
+  chave = "IAC-PROD"
+}
+
+output "IP" {
+  value = module.aws-prod.IP_publico
+  
+}
