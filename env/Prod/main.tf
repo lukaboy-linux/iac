08c7f0e7a333b922bc4290dcbc8620fa1aa4ad65
lukaboy-linux <lukaboy.ccz@gmail.com>
2023-07-30 15:49:28 -0300

    Aula 2 - Auto-Scalling

--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -3,9 +3,8 @@ module "aws-prod" {
   instancia = "t2.micro"
   regiao_aws = "us-east-1"
   chave = "IAC-PROD"
-}
-
-output "IP" {
-  value = module.aws-prod.IP_publico
-  
+  grupoDeSeguranca = "Producao"
+  minimo = 1
+  maximo = 10
+  nomeGrupo = "Prod"
 }
